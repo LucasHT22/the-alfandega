44aa9ea7b6712d1a6455b45062a540a4113e809a
Lucas Honda <88567622+LucasHT22@users.noreply.github.com>
2024-03-27 21:53:37 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
 
-export default defineConfig({
-  // ...
-  output: 'server',
-  adapter: vercel(),
-});
+// https://astro.build/config
+export default defineConfig({});
